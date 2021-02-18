28654a934b58cad3af3e3971ac997997155d33d3
Under-Sun <anderson54rus@gmail.com>
2021-02-18 17:57:23 +0700

    DZ # 5!

---
+++ src/lesson05/online/Bird.java
@@ -0,0 +1,14 @@
+package lesson05.online;
+
+public class Bird extends Animals {
+
+
+    public Bird(String name,float maxRun, float maxSwim, float maxJump) {
+        super(name, maxRun, maxSwim, maxJump);
+      //  System.out.println("Птица пробежала " + run + "м");
+    }
+    @Override
+    public int swim(float FullTrack){
+        return Animals.noSwim;
+    }
+}
---
+++ src/lesson05/online/Dog.java
@@ -0,0 +1,12 @@
+package lesson05.online;
+
+public class Dog extends Animals {
+
+
+    public Dog (String name,float maxRun, float maxSwim, float maxJump) {
+
+        super(name, maxRun, maxSwim, maxJump);
+     //   System.out.println("Пёсик пробежал " + run + "м");
+    }
+
+}
